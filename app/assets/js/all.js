916af5e8412be0dd364cc62cf4d30ab8317bd139
hsinyun <ruby8941@gmail.com>
2021-08-24 22:36:01 +0800

    [int] gulp init

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,12 +1,3 @@
-$(document).ready(() => {
-  let pathname = window.location.pathname;
-  $('.nav-bar__list-item > .nav-bar__item-link[href="' + pathname + '"]')
-    .parent()
-    .addClass('active');
-
-  $('.slide-content').hide();
-  $('.slide-button').click(function () {
-    $('.slide-button__icon').toggleClass('active');
-    $('.slide-content').slideToggle();
-  });
+$(function() {
+  console.log('Hello Bootstrap5');
 });
--- dist/assets/js/all.js
+++ dist/assets/js/all.js
@@ -1,12 +1,6 @@
 "use strict";
 
-$(document).ready(function () {
-  var pathname = window.location.pathname;
-  $('.nav-bar__list-item > .nav-bar__item-link[href="' + pathname + '"]').parent().addClass('active');
-  $('.slide-content').hide();
-  $('.slide-button').click(function () {
-    $('.slide-button__icon').toggleClass('active');
-    $('.slide-content').slideToggle();
-  });
+$(function () {
+  console.log('Hello Bootstrap5');
 });
 //# sourceMappingURL=all.js.map
